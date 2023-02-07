04a70558574124fb5879e040d03ed6ea6bf03d64
Vada237 <denis.le01@mail.ru>
2023-02-07 17:21:07 +0300

    registartion/login tests

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_new_user_can_register()
+    {
+        $response = $this->post('/api/auth/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'testpass'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'name',
+            'email',
+            'password',
+            'avatar'
+        ]);
+    }
+
+    public function test_new_user_can_register_without_password()
+    {
+        $response = $this->post('api/auth/register', [
+            'name' => 'Test user',
+            'email' => 'test@example2.com'
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_new_user_can_register_with_short_email() {
+        $response = $this->post('api/auth/register', [
+            'name' => 'Test user',
+            'email' => 't@m.r',
+            'password' => 'testpass'
+        ]);
+
+        $response->assertStatus(302);
+
+    }
+
+    public function test_login_user() {
+        $response = $this->post('/api/auth/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'testpass'
+        ]);
+
+        $response = $this->post('api/auth/login', [
+          "email" => "test@example.com",
+          "password" => "testpass"
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertStatus(200);
+    }
+}
