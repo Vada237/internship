b4a3e33486085d625c1941c1631cf7e5019ed4f5
Vada237 <denis.le01@mail.ru>
2023-02-07 17:26:27 +0300

    add userRequest

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:2',
+            'email' => 'required|min:6',
+            'password' => 'required|min:6',
+            'avatar' => 'image|mimes:jpeg,bmp,png'
+        ];
+    }
+}
