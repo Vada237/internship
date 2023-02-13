48e3577d564a400e5b729ac2f5623409d287b94d
Vada237 <denis.le01@mail.ru>
2023-02-13 17:44:36 +0300

    add organization_routes

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::controller(Api\UserController::class)->group(function() {
     Route::middleware('auth:sanctum')->get('user', [Api\UserController::class, 'index']);
     Route::middleware('auth:sanctum')->get('user/{id}', [Api\UserController::class, 'show']);
     Route::middleware('auth:sanctum')->delete('user/{id}', [Api\UserController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->patch('user/update', [Api\UserController::class, 'update']);
 });
 
 Route::controller(Api\AuthController::class)->group(function() {
@@ -30,4 +31,13 @@ Route::controller(Api\AuthController::class)->group(function() {
 Route::controller(Api\PasswordController::class)->group(function () {
    Route::post('password/forgot', [Api\PasswordController::class, 'forgot'])->middleware('guest')->name('password.email');
    Route::post('password/reset', [Api\PasswordController::class, 'reset'])->middleware('guest')->name('password.reset');
+});
+
+Route::controller(Api\OrganizationController::class)->group(function () {
+   Route::middleware('auth:sanctum')->get('organization', [Api\OrganizationController::class, 'index']);
+   Route::middleware('auth:sanctum')->get('organization/id={id}', [Api\OrganizationController::class, 'show']);
+   Route::middleware('auth:sanctum')->get('organization/name={name}', [Api\OrganizationController::class, 'getByName']);
+   Route::middleware('auth:sanctum')->post('organization/create', [Api\OrganizationController::class, 'store']);
+   Route::middleware('auth:sanctum')->patch('organization/{id}', [Api\OrganizationController::class, 'update']);
+   Route::middleware('auth:sanctum')->delete('organization/delete/{id}', [Api\OrganizationController::class, 'destroy']);
 });
