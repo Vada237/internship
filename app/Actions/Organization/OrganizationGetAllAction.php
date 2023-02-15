32b61444527da5259cd4ec61287b26370aa1d1b6
Vada237 <denis.le01@mail.ru>
2023-02-15 18:02:23 +0300

    add limit&offset parameters

--- app/Actions/Organization/OrganizationGetAllAction.php
+++ app/Actions/Organization/OrganizationGetAllAction.php
@@ -5,9 +5,13 @@ namespace App\Actions\Organization;
 use App\Http\Resources\OrganizationResource;
 use App\Models\Organization;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class OrganizationGetAllAction {
-    public function handle() {
-        return Organization::paginate(5);
+    public function handle(int $limit,int $offset) {
+        return DB::table('organizations')
+            ->limit($limit)
+            ->offset($offset)
+            ->get();
     }
 }
--- app/Actions/User/UserGetAllAction.php
+++ app/Actions/User/UserGetAllAction.php
@@ -5,9 +5,13 @@ namespace App\Actions\User;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserGetAllAction {
-    public function handle() {
-        return User::paginate(5);
+    public function handle(int $limit,int $offset) {
+        return DB::table('users')
+            ->offset($offset)
+            ->limit($limit)
+            ->get();
     }
 }
