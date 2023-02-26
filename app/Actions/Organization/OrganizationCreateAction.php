8150e6e183c5059bd6eca01e41ab31bab577e58e
Denis le <denis.le01@mail.ru>
2023-02-26 21:38:15 +0300

    fixed formatiing

--- app/Actions/Organization/OrganizationCreateAction.php
+++ app/Actions/Organization/OrganizationCreateAction.php
@@ -11,9 +11,8 @@ use App\Models\UserOrganization;
 use Illuminate\Support\Facades\Auth;
 
 class OrganizationCreateAction {
-    public function handle($credentials,User $user) {
-        $organization = new Organization($credentials);
-        $organization->save();
+    public function handle($params,User $user) {
+        $organization = Organization::create($params);        
         $user->organizations()->attach($organization->id,['role_id' => Role::where('slug', 'organization-supervisor')->first()->id]);
         return $organization;
     }
--- app/Actions/Organization/OrganizationUpdateAction.php
+++ app/Actions/Organization/OrganizationUpdateAction.php
@@ -10,9 +10,9 @@ use App\Models\UserOrganization;
 use Illuminate\Support\Facades\Auth;
 
 class OrganizationUpdateAction {
-    public function handle($credentials, int $organization_id,User $user) {
+    public function handle($params, int $organization_id,User $user) {
         $organization = $user->organizations()->findOrFail($organization_id);
-        $organization->update($credentials);
+        $organization->update($params);
         return $organization;
     }
 }
