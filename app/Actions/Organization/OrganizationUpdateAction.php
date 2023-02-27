2fb9155597293f2962ed193dac5dc8471b9824b6
Vada237 <denis.le01@mail.ru>
2023-02-27 18:07:55 +0300

    refactoring actions

--- app/Actions/Organization/OrganizationUpdateAction.php
+++ app/Actions/Organization/OrganizationUpdateAction.php
@@ -10,8 +10,7 @@ use App\Models\UserOrganization;
 use Illuminate\Support\Facades\Auth;
 
 class OrganizationUpdateAction {
-    public function handle($params, int $organization_id,User $user) {
-        $organization = $user->organizations()->findOrFail($organization_id);
+    public function handle($params, Organization $organization) {
         $organization->update($params);
         return $organization;
     }
