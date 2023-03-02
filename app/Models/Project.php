7ab216f19a4acaea0965de722a8c9fb130dac9a1
Vada237 <denis.le01@mail.ru>
2023-03-02 18:19:48 +0300

    update Organization&Project models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,16 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'name',
         'organization_id'
-        ];
+    ];
 
-    public function users() {
+    public function users()
+    {
         return $this->belongsToMany(User::class, 'users', 'user_id', 'project_id');
     }
 
-    public function roles() {
+    public function roles()
+    {
         return $this->belongsToMany(Role::class, 'roles', 'role_id', 'project_id');
+    }
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
     }
 }
