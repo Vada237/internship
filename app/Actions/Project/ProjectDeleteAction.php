532ef1c6c8cfc442bb3e10e35a9a48268912efb6
Vada237 <denis.le01@mail.ru>
2023-03-03 17:55:25 +0300

    added delete project action

--- app/Actions/Project/ProjectDeleteAction.php
+++ app/Actions/Project/ProjectDeleteAction.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\Actions\Project;
+
+use App\Models\Project;
+
+class ProjectDeleteAction {
+    public function handle(Project $project) {
+        $project->delete();
+        return __('messages.projects.delete.success');
+    }
+}
