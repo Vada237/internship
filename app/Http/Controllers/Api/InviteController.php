a7dc545f2f9b3d986d960f21e726001f1eef225e
Vada237 <denis.le01@mail.ru>
2023-03-07 17:41:10 +0300

    updated name params

--- app/Http/Controllers/Api/InviteController.php
+++ app/Http/Controllers/Api/InviteController.php
@@ -2,25 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Actions\Invite\InviteAcceptAction;
-use App\Actions\Invite\InviteSendAction;
+use App\Actions\Invite\Organization\OrganizationInviteAcceptAction;
+use App\Actions\Invite\Organization\OrganizationInviteSendAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Invite\InviteAcceptRequest;
 use App\Http\Requests\Invite\InviteSendRequest;
 use App\Models\Invite;
 use App\Models\Organization;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class InviteController extends Controller
 {
-    public function send(InviteSendAction $action, InviteSendRequest $request)
+    public function send(OrganizationInviteSendAction $action, InviteSendRequest $request)
     {
         $this->authorize('send', [Invite::class, Organization::find($request->organizationId)]);
         return $action->handle($request->validated(), Auth::user());
     }
 
-    public function accept(InviteAcceptAction $action, string $token)
+    public function accept(OrganizationInviteAcceptAction $action, string $token)
     {
         $this->authorize('accept', Invite::where('token', $token)->first());
         return $action->handle($token);
