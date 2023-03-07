767b88049ae70cfc9a72f3cca65561b76881e7e9
Vada237 <denis.le01@mail.ru>
2023-03-07 17:38:27 +0300

    rename files

---
+++ app/Actions/Invite/Organization/OrganizationInviteAcceptAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions\Invite\Organization;
+
+use App\Models\Invite;
+use App\Models\Role;
+use App\Models\User;
+
+
+class OrganizationInviteAcceptAction
+{
+    public function handle(string $token)
+    {
+        $invite = Invite::where('token', $token)->firstOrFail();
+        $user = User::byEmail($invite->email)->firstOrFail();
+
+        $user->organizations()->attach($invite->invitable_id,
+            ['role_id' => Role::byName(Role::list['EMPLOYEE'])->first()->id]);
+
+        $invite->delete();
+        return __('messages.organizations.join.success');
+    }
+}
