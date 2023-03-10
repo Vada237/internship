14b9828128f52b0e176ffbb1a9920848a727dc11
Vada237 <denis.le01@mail.ru>
2023-03-10 10:35:49 +0300

    updated routes

--- routes/api.php
+++ routes/api.php
@@ -50,8 +50,13 @@ Route::controller(Api\InviteController::class)->middleware('auth:sanctum')->grou
 });
 
 Route::controller(Api\ProjectController::class)->middleware('auth:sanctum')->group(function () {
+<<<<<<< Updated upstream
     Route::get('projects/', [Api\ProjectController::class, 'index']);
     Route::get('projects/findByOrganization/{organization}', [Api\ProjectController::class, 'getByOrganization']);
+=======
+    Route::get('projects', [Api\ProjectController::class, 'index']);
+    Route::get('projects/find_by_organization/{organization}', [Api\ProjectController::class, 'getByOrganization']);
+>>>>>>> Stashed changes
     Route::get('projects/{project}', [Api\ProjectController::class, 'show']);
     Route::post('projects', [Api\ProjectController::class, 'store']);
     Route::patch('projects/{project}', [Api\ProjectController::class, 'update']);
