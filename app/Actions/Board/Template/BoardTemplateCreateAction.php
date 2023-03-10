724f00561c9419566ac558cb8c915f8b401ccd21
Vada237 <denis.le01@mail.ru>
2023-03-10 17:49:58 +0300

    added task_template actions and update board create action

--- app/Actions/Board/Template/BoardTemplateCreateAction.php
+++ app/Actions/Board/Template/BoardTemplateCreateAction.php
@@ -3,12 +3,16 @@
 namespace App\Actions\Board\Template;
 
 use App\Models\BoardTemplate;
+use App\Models\User;
 
 class BoardTemplateCreateAction
 {
-    public function handle($params)
+    public function handle($params, int $userId)
     {
-        $boardTemplate = BoardTemplate::create($params);
+        $boardTemplate = BoardTemplate::create([
+            'name' => $params['name'],
+            'user_id' => $userId
+        ]);
 
         return $boardTemplate;
     }
---
+++ app/Actions/Task/Template/TaskTemplateCreateAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Actions\Task\Template;
+
+use App\Models\TaskTemplate;
+
+class TaskTemplateCreateAction {
+    public function handle($params)
+    {
+        return TaskTemplate::create($params);
+    }
+}
