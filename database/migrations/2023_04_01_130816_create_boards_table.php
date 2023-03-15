6846a8bf329b16cee71069b1906f025dc1a9c688
Vada237 <denis.le01@mail.ru>
2023-03-15 15:49:24 +0300

    renamed files

---
+++ database/migrations/2023_04_01_130816_create_boards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('project_id');
+            $table->string('status');
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+};
---
+++ database/migrations/2023_04_01_131134_create_commentaries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commentaries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('task_id');
+            $table->string('text');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('task_id')->references('id')->on('tasks')->cascadeOnDelete();
+
+            $table->unique(['user_id','task_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commentaries');
+    }
+};
