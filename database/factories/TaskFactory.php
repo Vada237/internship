fc688990681f37b7385b5221c814b7c88976a8c3
Vada237 <denis.le01@mail.ru>
2023-03-16 17:11:38 +0300

    added task\subtask factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Board;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->word,
+            'board_id' => Board::inRandomOrder()->first()->id
+        ];
+    }
+}
