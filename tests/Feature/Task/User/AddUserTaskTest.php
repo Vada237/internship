28cba8677a024ed4bbdc800150cc4eea90a7260b
Vada237 <denis.le01@mail.ru>
2023-03-17 16:17:24 +0300

    added add&delete user to task tests

---
+++ tests/Feature/Task/User/AddUserTaskTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Feature\Task\User;
+
+use App\Models\Board;
+use App\Models\BoardTemplate;
+use App\Models\Organization;
+use App\Models\Project;
+use App\Models\Role;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AddUserTaskTest extends TestCase
+{
+    public function testAddUserTaskSuccess()
+    {
+        $user = User::factory()->create();
+        $organization = Organization::factory()->create();
+        $project = Project::factory()->create();
+        $invitedUser = User::factory()->create();
+
+        $user->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::ORGANIZATION_SUPERVISOR)->first()->id]);
+
+        $user->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_SUPERVISOR)->first()->id]);
+
+        $invitedUser->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::USER)->first()->id]);
+
+        $invitedUser->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_EXECUTOR)->first()->id]);
+
+        $board = Board::create([
+            'name' => 'board',
+            'project_id' => $project->id,
+            'status' => Board::EDITED
+        ]);
+
+        $task = Task::create([
+            'name' => 'task',
+            'board_id' => $board->id
+        ]);
+
+        $response = $this->actingAs($user)->postJson('api/tasks/users', [
+            'user_id' => $invitedUser->id,
+            'task_id' => $task->id
+        ]);
+        $response->assertOk();
+        $this->assertDatabaseHas('user_tasks', [
+            'user_id' => $invitedUser->id,
+            'task_id' => $task->id
+        ]);
+    }
+
+    public function testAddUserTaskForbidden()
+    {
+        $user = User::factory()->create();
+        $organization = Organization::factory()->create();
+        $project = Project::factory()->create();
+        $invitedUser = User::factory()->create();
+
+        $invitedUser->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::USER)->first()->id]);
+
+        $invitedUser->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_EXECUTOR)->first()->id]);
+
+        $user->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::EMPLOYEE)->first()->id]);
+
+        $user->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_PARTICIPANT)->first()->id]);
+
+        $board = Board::create([
+            'name' => 'board',
+            'project_id' => $project->id,
+            'status' => Board::EDITED
+        ]);
+
+        $task = Task::create([
+            'name' => 'task',
+            'board_id' => $board->id
+        ]);
+
+        $response = $this->actingAs($user)->postJson('api/tasks/users', [
+            'user_id' => $invitedUser->id,
+            'task_id' => $task->id
+        ]);
+
+        $response->assertForbidden();
+    }
+
+    public function testAddUserTaskUnauthorized()
+    {
+        $organization = Organization::factory()->create();
+        $project = Project::factory()->create();
+        $invitedUser = User::factory()->create();
+
+        $invitedUser->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::USER)->first()->id]);
+
+        $invitedUser->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_EXECUTOR)->first()->id]);
+
+        $board = Board::create([
+            'name' => 'board',
+            'project_id' => $project->id,
+            'status' => Board::EDITED
+        ]);
+
+        $task = Task::create([
+            'name' => 'task',
+            'board_id' => $board->id
+        ]);
+
+        $response = $this->postJson('api/tasks/users', [
+            'user_id' => $invitedUser->id,
+            'task_id' => $task->id,
+        ]);
+
+        $response->assertUnauthorized();
+    }
+}
---
+++ tests/Feature/Task/User/DeleteUserTaskTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Tests\Feature\Task\User;
+
+use App\Models\Board;
+use App\Models\Organization;
+use App\Models\Project;
+use App\Models\Role;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DeleteUserTaskTest extends TestCase
+{
+    public function testDeleteUserTaskSuccess()
+    {
+        $user = User::factory()->create();
+        $organization = Organization::factory()->create();
+        $project = Project::factory()->create();
+        $invitedUser = User::factory()->create();
+
+        $user->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::ORGANIZATION_SUPERVISOR)->first()->id]);
+
+        $user->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_SUPERVISOR)->first()->id]);
+
+        $invitedUser->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::USER)->first()->id]);
+
+        $invitedUser->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_EXECUTOR)->first()->id]);
+
+        $board = Board::create([
+            'name' => 'board',
+            'project_id' => $project->id,
+            'status' => Board::EDITED
+        ]);
+
+        $task = Task::create([
+            'name' => 'task',
+            'board_id' => $board->id
+        ]);
+
+        $task->users()->attach($invitedUser->id);
+
+        $response = $this->actingAs($user)->deleteJson('api/tasks/users' . '/' . $task->id . '/' . $invitedUser->id);
+
+        $response->assertOk();
+        $this->assertDatabaseMissing('user_tasks', [
+            'user_id' => $invitedUser->id,
+            'task_id' => $task->id
+        ]);
+    }
+
+    public function testDeleteUserTaskForbidden()
+    {
+        $user = User::factory()->create();
+        $organization = Organization::factory()->create();
+        $project = Project::factory()->create();
+        $invitedUser = User::factory()->create();
+
+        $user->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::EMPLOYEE)->first()->id]);
+
+        $user->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_PARTICIPANT)->first()->id]);
+
+        $invitedUser->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::USER)->first()->id]);
+
+        $invitedUser->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_EXECUTOR)->first()->id]);
+
+        $board = Board::create([
+            'name' => 'board',
+            'project_id' => $project->id,
+            'status' => Board::EDITED
+        ]);
+
+        $task = Task::create([
+            'name' => 'task',
+            'board_id' => $board->id
+        ]);
+
+        $task->users()->attach($invitedUser->id);
+
+        $response = $this->actingAs($user)->deleteJson('api/tasks/users' . '/' . $task->id . '/' . $invitedUser->id);
+
+        $response->assertForbidden();
+    }
+
+    public function testDeleteUserTaskUnauthorized()
+    {
+        $organization = Organization::factory()->create();
+        $project = Project::factory()->create();
+        $invitedUser = User::factory()->create();
+
+        $invitedUser->organizations()->attach($organization->id,
+            ['role_id' => Role::byName(Role::USER)->first()->id]);
+
+        $invitedUser->projects()->attach($project->id,
+            ['role_id' => Role::byName(Role::PROJECT_EXECUTOR)->first()->id]);
+
+        $board = Board::create([
+            'name' => 'board',
+            'project_id' => $project->id,
+            'status' => Board::EDITED
+        ]);
+
+        $task = Task::create([
+            'name' => 'task',
+            'board_id' => $board->id
+        ]);
+
+        $task->users()->attach($invitedUser->id);
+
+        $response = $this->deleteJson('api/tasks/users' . '/' . $task->id . '/' . $invitedUser->id);
+
+        $response->assertUnauthorized();
+    }
+}
